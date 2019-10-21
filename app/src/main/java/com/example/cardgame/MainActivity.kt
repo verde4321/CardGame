b9eae882e7af612c9a0a9d555e82782f21c2d44a
Fabius <Fabius@DESKTOP-HFI2MNB>
2019-10-21 09:54:36 +0200

    main menue layout

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.cardgame
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
+
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_menu)
+
+
+        val imageButton = findViewById<ImageButton>(R.id.menuButton)
+        imageButton?.setOnClickListener { Toast.makeText(this@MainActivity, "fuck this schit i am out", Toast.LENGTH_SHORT).show()
+            startMenuActivity()
+        }
+
+
     }
+
+    fun startMenuActivity() {
+
+
+        val intent = Intent(this, MenuSettings::class.java)
+            intent.putExtra("key", "gg")
+
+        this.startActivity(intent)
+
+
+    }
+
 }
+
---
+++ app/src/main/java/com/example/cardgame/MenuSettings.kt
@@ -0,0 +1,18 @@
+package com.example.cardgame
+
+
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class MenuSettings : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.menu)
+        }
+
+}
