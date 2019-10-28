0843d86f8afefd16c5d9b168b9d6687f4f240f25
Fabius <Fabius@DESKTOP-HFI2MNB>
2019-10-28 16:54:48 +0100

    Privacy Policy is done

---
+++ app/src/main/java/com/example/cardgame/Privacy_Policy.kt
@@ -0,0 +1,26 @@
+package com.example.cardgame
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class Privacy_Policy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.privacy_policy)
+
+        val linkTextView = findViewById<TextView>(R.id.link)
+        linkTextView?.setOnClickListener {
+            privacyFromGoogle()
+        }
+
+    }
+    fun privacyFromGoogle(){
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://policies.google.com/privacy"))
+        startActivity(i)
+
+    }
+}
