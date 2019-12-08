75fe5f9124f7e4ed8ae80174778ba03c667b12bb
TechnicianLP <9272536+TechnicianLP@users.noreply.github.com>
2019-12-08 19:24:02 +0100

    CG-31 Move Reviews to Main Project - Fix Tests

--- app/src/test/java/com/tinf18b2/cardgame/model/cards/MockCard.kt
+++ app/src/test/java/com/tinf18b2/cardgame/model/cards/MockCard.kt
@@ -1,6 +1,6 @@
 package com.tinf18b2.cardgame.model.cards
 
-enum class MockCard constructor(override val cardName: String, override val resourceID: Int) : ICard {
+enum class MockCard constructor(override val cardName: String, val resourceID: Int) : ICard {
     ONE("ONE", 1),
     TWO("TWO", 2),
     THREE("THREE", 3),
--- app/src/test/java/com/tinf18b2/cardgame/model/cards/stack/IStackTest.kt
+++ app/src/test/java/com/tinf18b2/cardgame/model/cards/stack/IStackTest.kt
@@ -1,27 +1,9 @@
 package com.tinf18b2.cardgame.model.cards.stack
 
-import com.tinf18b2.cardgame.R
-import com.tinf18b2.cardgame.model.cards.MockCard
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class IStackTest {
 
-    @Test
-    fun testGetResourceIDEmpty() {
-        val test = MockStack<MockCard>(false, mutableListOf())
-        assertEquals(R.drawable.target_frame, test.resourceID)
-    }
 
-    @Test
-    fun testGetResourceIDHidden() {
-        val test = MockStack(false, mutableListOf(MockCard.ONE))
-        assertEquals(R.drawable.red_joker, test.resourceID) //TODO backside drawable
-    }
-
-    @Test
-    fun testGetResourceIDShown() {
-        val test = MockStack(true, mutableListOf(MockCard.ONE))
-        assertEquals(MockCard.ONE.resourceID, test.resourceID)
-    }
 }
